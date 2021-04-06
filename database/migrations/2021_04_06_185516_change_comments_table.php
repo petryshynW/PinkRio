aa7d3a13d43a64312e170b7bb8c002d59cf3a7be
petryshynW <perdol12>
2021-04-06 22:24:14 +0300

    migrations

---
+++ database/migrations/2021_04_06_185516_change_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->foreignId('article_id')->constrained('articles');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
